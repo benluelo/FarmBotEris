25ccb77d58a19e2bc002e2f00893871e756f3762
Tyler <tylersmaldonado@gmail.com>
2020-01-30 17:40:41 -0500

    trying to fix the error event

--- bot/events/error.js
+++ bot/events/error.js
@@ -1,3 +1,8 @@
 exports.run = async (bot, err, id) => {
-  bot.log.error(JSON.stringify(err, null, 4) + "\nERROR ID: " + id)
+  // do this for now to see what `err` makes. I don't believe you can
+  // `JSON.stringify(err, null, 4)` the error that is made
+  // also `id` is the shard id not the error id
+  console.log(`error: ${err}`)
+  console.log(`id: ${id}`)
+  //bot.log.error(JSON.stringify(err, null, 4) + "\nERROR ID: " + id)
 }
