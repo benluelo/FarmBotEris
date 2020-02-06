d928c9aeaae45837951f971fc9a4eb90687f77d3
Ben <benluelo@hotmail.com>
2020-02-05 19:51:45 -0500

    yes

--- bot/commands/harvest.js
+++ bot/commands/harvest.js
@@ -17,14 +17,14 @@ module.exports.run = async (bot) => {
         for (let plot in farm) {
 
           const userCrop = farm[plot].crop
-      
+
           // console.log(chalk.bold.keyword("brown")(`FARM PLOT #${plot}:\n`), farm[plot])
-      
+
           if ((userCrop.planted != "dirt") && ((Date.now() - userCrop.datePlantedAt) >= bot.config.farminfo.growTimes[userCrop.planted])){
-      
+
             // console.log("TEST BEFORE:", plot, userCrop.planted)
-      
-            await bot.database.Userdata.findOneAndUpdate({ userID: message.author.id }, 
+
+            await bot.database.Userdata.findOneAndUpdate({ userID: message.author.id },
               {
                 $set: {
                   [`farm.${plot}.crop.planted`]: "dirt",
@@ -38,22 +38,22 @@ module.exports.run = async (bot) => {
             ).then((res) => {
               totalPlots += 1
               value += 1
-      
+
               // console.log("TEST AFTER:", plot, res.value.userCrop.planted)
               // console.log(chalk.bold.blue(`PLOT #${plot} HARVESTED`))
-      
+
             }).catch(err => {
               console.log(err.message)
             })
-      
+
             // console.log("test:", plot)
-      
+
           }
-      
+
         }
-                  
+
         // console.log(chalk.yellow.underline(`${totalPlots} plots harvested in total`))
-      
+
         bot.createMessage(
           message.channel.id,
           `${totalPlots} plots harvested in total, you made ${value} <:farmbot_coin:648032810682023956> selling the crops!`)
