bffbb2dfd68c419a1becff721ba44af3b56c0034
Ben <benluelo@hotmail.com>
2020-02-05 20:00:17 -0500

    yes (again)

--- bot/commands/harvest.js
+++ bot/commands/harvest.js
@@ -12,24 +12,6 @@ module.exports.run = async (bot) => {
         let farm = userdata.farm
         let totalPlots = 0
 
-<<<<<<< HEAD
-          const userCrop = farm[plot].crop
-
-          // console.log(chalk.bold.keyword("brown")(`FARM PLOT #${plot}:\n`), farm[plot])
-
-          if ((userCrop.planted != "dirt") && ((Date.now() - userCrop.datePlantedAt) >= bot.config.farminfo.growTimes[userCrop.planted])){
-
-            // console.log("TEST BEFORE:", plot, userCrop.planted)
-
-            await bot.database.Userdata.findOneAndUpdate({ userID: message.author.id },
-              {
-                $set: {
-                  [`farm.${plot}.crop.planted`]: "dirt",
-                  [`farm.${plot}.crop.datePlantedAt`]: Date.now(),
-                },
-                $inc: {
-                  money: 1
-=======
         // harvest all plots
         if (!args[0]) {
           for (let plot in farm) {
@@ -47,7 +29,6 @@ module.exports.run = async (bot) => {
                   $inc: {
                     [`seeds.common.${userCrop.planted}.amount`]: 1
                   }
->>>>>>> e5509497b814d66b6d8a71df5f2c898ea4bdf5db
                 }
               ).then(() => {
                 totalPlots += 1
@@ -91,36 +72,10 @@ module.exports.run = async (bot) => {
               } else {
                 bot.createMessage(message.channel.id, "Cannot harvest dirt!")
               }
-<<<<<<< HEAD
-              // eslint-disable-next-line no-unused-vars
-            ).then((res) => {
-              totalPlots += 1
-              value += 1
-
-              // console.log("TEST AFTER:", plot, res.value.userCrop.planted)
-              // console.log(chalk.bold.blue(`PLOT #${plot} HARVESTED`))
-
-            }).catch(err => {
-              console.log(err.message)
-            })
-
-            // console.log("test:", plot)
-
-          }
-
-        }
-
-        // console.log(chalk.yellow.underline(`${totalPlots} plots harvested in total`))
-
-        bot.createMessage(
-          message.channel.id,
-          `${totalPlots} plots harvested in total, you made ${value} <:farmbot_coin:648032810682023956> selling the crops!`)
-=======
             }
           }
 
         }
->>>>>>> e5509497b814d66b6d8a71df5f2c898ea4bdf5db
       }
     })
 
