a705ddf41bb793d3f07b7250189763eb9acae9f1
Ben <benluelo@hotmail.com>
2020-02-09 19:47:29 -0500

    Fix planting over crops when planting one at a time

--- bot/commands/plant.js
+++ bot/commands/plant.js
@@ -19,12 +19,14 @@ exports.run = (bot) => {
         if (plotNumber !== false && truePlant) {
 
           if (!userdata) {
-            bot.startMessage(message)
+            return bot.startMessage(message)
           }
           if (userdata) {
             if (plotNumber >= userdata.farm.length){
-              bot.createMessage(message.channel.id, "You don't own that plot!")
-              return
+              return bot.createMessage(message.channel.id, "You don't own that plot!")
+            }
+            if (userdata.farm[plotNumber].crop.planted != "dirt") {
+              return bot.createMessage(message.channel.id, `There's already a crop planted on plot \`${plot}\`!`)
             }
             await bot.database.Userdata.findOneAndUpdate({ userID: message.author.id },
               {
@@ -34,30 +36,28 @@ exports.run = (bot) => {
                 }
               }
             )
-            bot.createMessage(message.channel.id, `Planted ${bot.plants[crop]} on \`${plot}\`!`)
+            return bot.createMessage(message.channel.id, `Planted ${bot.plants[crop]} on \`${plot}\`!`)
           }
-
         } else {
-          bot.createMessage(message.channel.id, "Invalid input! Please try again with the format `<plant> <letter><number>`.")
+          return bot.createMessage(message.channel.id, "Invalid input! Please try again with the format `<plant> <letter><number>`.")
         }
       } else {
-        bot.createMessage(message.channel.id, "You have to specify a crop to plant!")
+        return bot.createMessage(message.channel.id, "You have to specify a crop to plant!")
       }
     })
-
   }, bot.cooldown(5000)).registerSubcommand("all", (message, args) => {
 
     bot.database.Userdata.findOne({ userID: message.author.id }, async (err, userdata) => {
       if (err) bot.log.error(err)
       if (!userdata) {
-        bot.startMessage(message)
+        return bot.startMessage(message)
       }
 
       if (!args[0]) return bot.createMessage(message.channel.id, "Please add the plant you want to plant")
-      if (!Object.keys(userdata.seeds.common).includes(args[0])) return bot.createMessage(message.channel.id, "Please include a vaild plant type")
-      if (!userdata.seeds.common[args[0]].discovered) return // slient quit
+      if (!bot.plants[args[0]]) return bot.createMessage(message.channel.id, "Please include a vaild plant type")
+      if (!userdata.seeds.common[args[0]].discovered) return // silent quit
 
-      bot.createMessage(message.channel.id, "Planting all!").then(async msg => {
+      return bot.createMessage(message.channel.id, "Planting all!").then(async msg => {
 
         let totalPlots = 0
 
