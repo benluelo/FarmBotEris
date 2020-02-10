a9f57e4f920ece9117fd88d8f4fe3720812593c9
Ben <benluelo@hotmail.com>
2020-02-09 19:19:21 -0500

    Add ping command

--- bot/commands/ping.js
+++ bot/commands/ping.js
@@ -1,5 +1,9 @@
 exports.run = (bot) => {
   // eslint-disable-next-line no-unused-vars
   bot.registerCommand("ping", (message, args) => {
+    const startTime = Date.now()
+    bot.createMessage(message.channel.id, "Pinging...").then((msg) => {
+      msg.edit("Pong! " + (Date.now() - startTime) + "ms.")
+    })
   }, bot.cooldown(5000))
 }
