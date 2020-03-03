86415695ae51485b85c6c987b26745d0053c44f7
Tyler <tylersmaldonado@gmail.com>
2020-03-03 04:20:13 -0500

    simple stuff

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config({ path: ".env" })
 const express = require("express")
+const fetch = require("node-fetch")
 const path = require("path")
 const exphbs = require("express-handlebars")
 const app = express()
@@ -7,16 +8,31 @@ const app = express()
 app.engine("handlebars", exphbs({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
 
-// homepage Route
-app.get("/", (req, res) => res.render("index", {
-  title: "FarmBot"
-}))
+// homepage route
+app.get("/", async (req, res) => {
+  res.render("index", {
+    title: "FarmBot Home"
+  })
+})
 
-// userdata Route
-app.get("/userdata", (req, res) => res.render("userdata", {
-  title: "UserData Test"
-}))
+// userdata page
+app.get("/userdata/:userID", async (req, res) => {
+  const givenUserID = req.params.userID
 
-app.listen(4999, () => {
+  let userdataget
+  await fetch(`http://localhost:5000/getUserData/${givenUserID}/1a40715b-8963-4eb2-bde9-b8d2c0b16cbf`).then(res => res.json()).then(json => userdataget = json)
 
+  res.render("userdata", {
+    title: "Userdata",
+    userdata: userdataget
+  })
+})
+
+// login to discord route
+app.get("/login", async (req, res) => {
+  res.redirect("https://discordapp.com/api/oauth2/authorize?client_id=646399118880669716&redirect_uri=http%3A%2F%2F73.194.182.148%3A4999%2Fcallback&response_type=code&scope=identify")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Web Server running on port: ${process.env.PORT}`)
 })
